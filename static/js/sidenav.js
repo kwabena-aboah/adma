ce77828633d7e4da7d1cc41112023656cdb3bec1
Mintah Obed Aboah <obed30mintah@yahoo.com>
2019-04-30 05:32:33 +0000

    Add files via upload

---
+++ static/js/sidenav.js
@@ -0,0 +1,15 @@
+  M.AutoInit();
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('.datepicker').datepicker();
+    $('.select').formSelect();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.modal').modal();
+    $('.tap-target').tapTarget();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $(".dropdown-trigger").dropdown();
+  });
+     
